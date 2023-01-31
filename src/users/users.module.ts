06400cc92f71417e608ba9730afee454ce0ce6f7
vedant-204 <katyayana2003@gmail.com>
2023-01-31 15:08:12 +0530

    user authentication feature addition

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { TypeOrmModule }from '@nestjs/typeorm';
+import user from './users.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([user])]
   providers: [UsersService]
+  exports: [UserService]
 })
 export class UsersModule {}
