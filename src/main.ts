4b9d23abf9ddb37683eb10cac0db946d6a9a787f
vedant-204 <katyayana2003@gmail.com>
2023-02-09 20:19:00 +0530

    serializing the interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(
+    app.get(Reflector)
+  ));
   app.use(cookieParser());
   await app.listen(3000);
 }
